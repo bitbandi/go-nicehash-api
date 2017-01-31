c1787eb37bda751f9301925cd6c7fa6f49f6cc8b
elbandi <bandispam@gmail.com>
2017-01-31 23:11:04 +0100

    Add order create call

--- orders.go
+++ orders.go
@@ -59,6 +59,34 @@ func (client *NicehashClient) GetMyOrders(algo AlgoType, location Location) ([]M
 	return stats.Result.Orders, nil
 }
 
+type NewOrder struct {
+	Algo       AlgoType `json:"algo" url:"algo"`
+	Price      float64 `json:"price,string" url:"price"`
+	Amount     float64 `json:"amount,string" url:"amount"`
+	PoolHost   string `json:"pool_host" url:"pool_host"`
+	PoolPort   uint16 `json:"pool_port" url:"pool_port"`
+	PoolUser   string `json:"pool_user" url:"pool_user"`
+	PoolPass   string `json:"pool_pass" url:"pool_pass"`
+	Alive      bool `json:"alive" url:"alive"`
+	LimitSpeed float64 `json:"limit,string" url:"limit,omitempty"`
+	Code       string `json:"code" url:"code,omitempty"`
+}
+
+func (client *NicehashClient) OrderCreate(order NewOrder) (string, error) {
+	stats := &struct {
+		Result struct {
+				Success string `json:"success"`
+			} `json:"result"`
+	}{}
+	params := &Params{Method:"orders.create", ApiId:client.apiid, ApiKey:client.apikey}
+	_, err := client.sling.New().Get("").QueryStruct(params).QueryStruct(order).ReceiveSuccess(&stats)
+	if err != nil {
+		return stats.Result.Success, err
+	}
+
+	return stats.Result.Success, nil
+}
+
 func (client *NicehashClient) OrderRefill(algo AlgoType, location Location, order uint, amount float64) (string, error) {
 	stats := &struct {
 		Result struct {
